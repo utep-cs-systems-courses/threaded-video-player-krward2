aafa333c78f469a10d74f3f45086b42604b96b42
krward2 <43589922+krward2@users.noreply.github.com>
2020-10-29 16:02:42 -0600

    Have the three functions. Working on queue.

---
+++ FrameQueue.py
@@ -0,0 +1,10 @@
+
+class FrameQueue:
+    __init__(self):
+        self.list = []
+
+    def push(frame, thread):
+        self.list.insert(frame, 0)
+
+    def pop(thread):
+        frame = self.list.pop(len(self.list))
