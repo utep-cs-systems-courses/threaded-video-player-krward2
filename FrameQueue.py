124aad33e3fe3489406c1eeb8e67f40e22d4532b
krward2 <43589922+krward2@users.noreply.github.com>
2020-10-29 17:29:40 -0600

    Queue first attempt

--- FrameQueue.py
+++ FrameQueue.py
@@ -1,10 +1,16 @@
 
 class FrameQueue:
-    __init__(self):
+    __init__(self, lock):
         self.list = []
+        self.lock = lock
 
     def push(frame, thread):
+        lock.acquire()
         self.list.insert(frame, 0)
+        lock.release()
 
     def pop(thread):
+        lock.acquire()
         frame = self.list.pop(len(self.list))
+        lock.release()
+        return frame
